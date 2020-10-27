0448bfd7f366841c331ae06fc33788a14401b7c4
Silviu Bogan <silviubogan@gmail.com>
2020-10-27 16:36:17 +0200

    Move code from applyConfig to matomo reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,26 @@
 import { GET_CONTENT } from '@plone/volto/constants/ActionTypes';
+import { createInstance } from '@datapunt/matomo-tracker-react';
 import { settings } from '~/config';
 
-export function matomo_reducer(state = {}, action = {}) {
+export function matomo(state = {}, action = {}) {
   switch (action.type) {
     case `${GET_CONTENT}_SUCCESS`:
-      if (__CLIENT__) {
+      if (__CLIENT__ && settings.matomoSiteId) {
+        const matomoInstance = settings.matomoInstance || createInstance({
+          urlBase: settings.matomoUrlBase || 'https://matomo.eea.europa.eu/',
+          siteId: settings.matomoSiteId, // optional, default value: `1`
+          // trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
+          // srcUrl: 'https://LINK.TO.DOMAIN/tracking.js', // optional, default value: `${urlBase}matomo.js`
+        });
+
         const content = action.result;
-        settings.matomoInstance.trackPageView({
+        const rv = matomoInstance.trackPageView({
           documentTitle: content.title || 'untitled',
           href: content['@id']
             .replace(settings.apiPath, '')
             .replace(settings.internalApiPath, ''),
         });
+        console.log('Y', { content, rv });
       }
       return state;
     default:
@@ -19,4 +28,4 @@ export function matomo_reducer(state = {}, action = {}) {
   }
 }
 
-export default matomo_reducer;
+export default matomo;
