18d062025cdeaff82a16d9d35083a7caed1498f2
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-12-02 17:29:14 +0200

    Improve implementation

--- src/MatomoAppExtra.jsx
+++ src/MatomoAppExtra.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { flattenToAppURL } from '@plone/volto/helpers';
+import { flattenToAppURL, getBaseUrl } from '@plone/volto/helpers';
 import { trackPageView } from './utils';
 
-export const MatomoAppExtra = (props) => {
-  const href = React.useMemo(() => {
-    return flattenToAppURL(props.content['@id']);
-  }, [props.content]);
+export const MatomoAppExtra = ({ location, content, ...rest }) => {
+  const { pathname } = location;
+
+  const href = flattenToAppURL(content['@id']);
+  const title = content?.title;
+  const baseUrl = getBaseUrl(pathname);
+  // console.log(href, pathname, `-${baseUrl}-`, baseUrl.length);
 
   React.useEffect(() => {
-    trackPageView({ href, documentTitle: document.title });
-  }, [href]);
+    if (href === pathname) {
+      // a document (content)
+      console.log('track doc', { href, documentTitle: title });
+      trackPageView({ href, documentTitle: title });
+    }
+    if (baseUrl && baseUrl !== pathname) {
+      // a route (utility view)
+      const action = pathname.split('/')[pathname.split('/').length - 1];
+      console.log('action', { href: pathname, documentTitle: action });
+      trackPageView({ href: pathname, documentTitle: action });
+    }
+  }, [href, pathname, title, baseUrl]);
 
   return <React.Fragment />;
 };
