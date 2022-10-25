de170d36cd8f400b72054620a1e34fcf2b20762b
Miu Razvan <miu.razvan28@gmail.com>
2022-10-25 11:33:40 +0300

    refactor: Upgrade to latest cypress + more config options

--- src/utils.js
+++ src/utils.js
@@ -10,31 +10,52 @@ import config from '@plone/volto/registry';
 const _matomo = {};
 
 const doWithMatomo = (fn) => {
-  if (__CLIENT__) {
-    if (!_matomo.instance) {
-      const siteId =
-        window.env?.RAZZLE_MATOMO_SITE_ID || config.settings.matomoSiteId;
+  if (__SERVER__) return;
+  if (!_matomo.instance) {
+    const siteId =
+      window.env?.RAZZLE_MATOMO_SITE_ID || config.settings.matomoSiteId;
 
-      const urlBase =
-        window.env?.RAZZLE_MATOMO_URL ||
-        config.settings.matomoUrlBase ||
-        'https://matomo.eea.europa.eu/';
+    const urlBase =
+      window.env?.RAZZLE_MATOMO_URL ||
+      config.settings.matomoUrlBase ||
+      'https://matomo.eea.europa.eu/';
 
-      if (siteId) {
-        _matomo.instance = createInstance({
-          urlBase,
-          siteId,
-        });
-      } else {
-        /* eslint-disable-next-line */
-        console.warn(
-          'Matomo SiteID is not defined, page actions will not be tracked',
-        );
-      }
+    const userId =
+      window.env?.RAZZLE_MATOMO_USER_ID || config.settings.matomoUserId;
+
+    const trackerUrl =
+      window.env?.RAZZLE_MATOMO_TRAKER_URL ||
+      config.settings.matomoTrackerUrl ||
+      `${urlBase}matomo.php`;
+
+    const srcUrl =
+      window.env?.RAZZLE_MATOMO_SRC_URL ||
+      config.settings.matomoSrcUrl ||
+      `${urlBase}matomo.js`;
+
+    if (siteId) {
+      /**
+       * NOTE: check this link to see all the available options
+       * https://www.npmjs.com/package/@datapunt/matomo-tracker-react
+       */
+      _matomo.instance = createInstance({
+        urlBase,
+        siteId,
+        userId,
+        trackerUrl,
+        srcUrl,
+        // Add your own configuration
+        ...(config.settings.matomo || {}),
+      });
+    } else {
+      /* eslint-disable-next-line */
+      console.warn(
+        'Matomo SiteID is not defined, page actions will not be tracked',
+      );
     }
-
-    if (_matomo.instance) fn(_matomo.instance);
   }
+
+  if (_matomo.instance) fn(_matomo.instance);
 };
 
 export const trackPageView = ({ href, ...options }) => {
